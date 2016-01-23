d33f08f217e67df752eb565cff5b67189f31dcbf
Jayce Rudig-Leathers <me@jayceleathers.com>
2016-01-23 15:10:49 -0700

    animate banner tweaks

--- js/animation.js
+++ js/animation.js
@@ -4,15 +4,18 @@
 $(function() {
 
 
-    var titles = ["Programmer" , "Artist", "Writer"];
+    var titles = ["Creative","Leader","Developer" , "Artist", "Writer", "Photographer","Human","Witch", ];
     var currentPos = 0;
     $('#animated_banner').text(titles[currentPos])
 
     function loop(){
       currentPos = (currentPos + 1 ) % titles.length
-      $('#animated_banner').fadeOut(1, function() {
-          $(this).text(titles[currentPos]).fadeIn(2000, loop);
-      });
+      setTimeout(function(){
+        $('#animated_banner').fadeOut(1, function() {
+            $(this).text(titles[currentPos]).fadeIn(1, loop);
+        });
+      }, 500s);
+
      }
 
      loop();
